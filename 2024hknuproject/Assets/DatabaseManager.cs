fba12d9d244b47874faf5b08ba2b44e9d31350fc
coachhknu <coach970407@gmail.com>
2024-07-06 03:30:23 +0900

    데이터베이스 초기버전(아이템,무기)

---
+++ 2024hknuproject/Assets/DatabaseManager.cs
@@ -0,0 +1,123 @@
+using System.Data;
+using Mono.Data.Sqlite;
+using UnityEngine;
+
+public class DatabaseManager : MonoBehaviour
+{
+    private string dbPath;
+
+    void Start()
+    {
+        dbPath = "URI=file:" + Application.persistentDataPath + "/GameDatabase.db";
+        CreateDatabase();
+        CreateTables();
+    }
+
+    void CreateDatabase()
+    {
+        if (!System.IO.File.Exists(dbPath))
+        {
+            SqliteConnection.CreateFile(dbPath);
+            Debug.Log("Database created at " + dbPath);
+        }
+    }
+
+    void CreateTables()
+    {
+        using (var connection = new SqliteConnection(dbPath))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                // 아이템 테이블 생성
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Items (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Description TEXT, Value INTEGER)";
+                command.ExecuteNonQuery();
+
+                // 무기 테이블 생성
+                command.CommandText = "CREATE TABLE IF NOT EXISTS Weapons (Id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, ProjectileSpeed REAL, Damage INTEGER, MagazineCapacity INTEGER, ProjectileShape TEXT)";
+                command.ExecuteNonQuery();
+
+                Debug.Log("Tables created successfully.");
+            }
+        }
+    }
+
+    public void InsertItem(string name, string description, int value)
+    {
+        using (var connection = new SqliteConnection(dbPath))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Items (Name, Description, Value) VALUES (@name, @description, @value)";
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@description", description);
+                command.Parameters.AddWithValue("@value", value);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void InsertWeapon(string name, float projectileSpeed, int damage, int magazineCapacity, string projectileShape)
+    {
+        using (var connection = new SqliteConnection(dbPath))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Weapons (Name, ProjectileSpeed, Damage, MagazineCapacity, ProjectileShape) VALUES (@name, @projectileSpeed, @damage, @magazineCapacity, @projectileShape)";
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@projectileSpeed", projectileSpeed);
+                command.Parameters.AddWithValue("@damage", damage);
+                command.Parameters.AddWithValue("@magazineCapacity", magazineCapacity);
+                command.Parameters.AddWithValue("@projectileShape", projectileShape);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+
+    public void GetItems()
+    {
+        using (var connection = new SqliteConnection(dbPath))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM Items";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log("Id: " + reader["Id"] + " Name: " + reader["Name"] + " Description: " + reader["Description"] + " Value: " + reader["Value"]);
+                    }
+                }
+            }
+        }
+    }
+
+    public void GetWeapons()
+    {
+        using (var connection = new SqliteConnection(dbPath))
+        {
+            connection.Open();
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM Weapons";
+
+                using (IDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.Log("Id: " + reader["Id"] + " Name: " + reader["Name"] + " ProjectileSpeed: " + reader["ProjectileSpeed"] + " Damage: " + reader["Damage"] + " MagazineCapacity: " + reader["MagazineCapacity"] + " ProjectileShape: " + reader["ProjectileShape"]);
+                    }
+                }
+            }
+        }
+    }
+}
